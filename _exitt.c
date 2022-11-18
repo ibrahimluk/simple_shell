c926fd03598a4832906ece9f893bc793bd020c98
ibrahim lukman <ibrahimlukman147@gmail.com>
2022-11-18 15:35:50 -0800

    taskk updated

---
+++ _exitt.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  _exitt - Handle exit command
+ *  @av: string
+ *  @num: num
+ *  Description: Handle exit command
+ *  Return: Integer
+ */
+int _exitt(char *av, int num)
+{
+	char *s = "exit";
+	int i = 0;
+
+	for (; av[i] > 96 && av[i] < 123 && av[i] == s[i]; i++)
+		;
+	if (i == 4)
+	{
+		free(av);
+		if (num == 512)
+			exit(2);
+		else
+			exit(num);
+	}
+	return (1);
+}
---
+++ _free.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _free - Free double pointer
+ * @s: Double pointer to free
+ *
+ * Description: Free double pointer
+ * Return: Nothing
+ */
+void _free(char **s)
+{
+	int j = 0;
+
+	while (s[j])
+		free(s[j++]);
+	free(s);
+}
