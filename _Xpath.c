26e69ded1b7395285ac4b86232ee96744e4f7453
adam <aadamu7@yahoo.com>
2022-11-22 14:01:38 +0100

    fixed findX

--- _Xpath.c
+++ _Xpath.c
@@ -4,16 +4,23 @@
  * _Xpath - handle the path
  * @p_find: p_find
  * @env: environment
+ * @av: description
+ * @cont: descrition
+ * @flag_1:given flags
+ * @num:descrition num
  * Return: av_0
  */
 char *_Xpath(char **av, int *p_find, char **env, int *cont,
 		int *flag_1, int *num)
 {
-	int i = 0, l_av, j;char *av_0;
+	int i = 0, l_av, j;
+	char *av_0;
 	char *av_0;
 
 	i = _strlen(av[0]), av_0 = malloc(sizeof(char) * (i + 1));
+
 	_getav0(&l_av, av, av_0);
+
 	for (j = 0; env[j] && access(av[0], X_OK) != 0 && *p_find == 0; j++)
 		_path(av, env[j], av_0, l_av, p_find);
 	if (access(av[0], X_OK) == 0)
